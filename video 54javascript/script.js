206a829ce4d45d563dd74ca3b370d09b688a720a
Raviprabhat527 <ravikumar312004@gmail.com>
2025-02-14 16:58:45 +0530

    video 54javascript

---
+++ video 54javascript/script.js
@@ -0,0 +1,23 @@
+alert("Hello world");
+
+console.log("Code is running...")
+console.log("Code is also running....")
+console.log("code is looling like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("computer is not blasting")
+}
+
+console.log("Your number is " + a)
+/*console.log("Your number is = " +a) */
+
+document.title="Hey I am a good"
+
+document.body.style.backgroundColor = "blue"
