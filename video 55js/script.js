1c177484de013b9d95c67e537a79c10beb2f951b
Raviprabhat527 <ravikumar312004@gmail.com>
2025-02-14 20:22:52 +0530

    video 55js

---
+++ video 55js/script.js
@@ -0,0 +1,47 @@
+console.log("Hey this is tutorial 60")
+
+var a = 5;
+a = a +1
+var b = 20;
+var c = "Ravi";
+var _a= "shubham";
+// var 55a = "Rohan"; // Not Allowes
+
+
+// console.log(a + b + 50);
+// console.log(typeof a, typeof b, typeof c, typeof _a);
+
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 1; //Not allowed because a1 is constant
+
+//primitive data type & object
+let x = "prabhat";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+// object
+let o = {
+  /*"name"*/ name: "Ravi",
+             "Jobe code": 5000,
+             "is_handsome":true
+}
+console.log(o);
+
+o.salary = "10crores";
+console.log(o);
+
+o.salary = "50crores";
+console.log(o);
