d9caa8b11afce82afa40779b84544348450a69d3
Raviprabhat527 <ravikumar312004@gmail.com>
2025-02-14 21:38:04 +0530

    Create script.js

---
+++ video 56js/script.js
@@ -0,0 +1,28 @@
+/* console.log("Hello I am consition tutorial")
+
+let age = 45;
+
+if(age>18){
+    console.log("You can Drive");
+}
+else{
+    console.log("You can not Drive");
+} */
+
+// Arithmetic Operator
+let ages = 3;
+let grace = 2;
+
+console.log(ages + grace)
+console.log(ages - grace)
+console.log(ages * grace)
+console.log(ages / grace)
+console.log(ages ** grace)
+console.log(ages % grace)
+
+if((ages+grace)>18){
+    console.log('You can drives')
+}
+else{
+    console.log('You can not drive')
+}
