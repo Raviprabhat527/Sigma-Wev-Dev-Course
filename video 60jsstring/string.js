2beeedc582b87ad6a21efd4006ab6177a43d1130
Raviprabhat527 <ravikumar312004@gmail.com>
2025-02-15 11:57:10 +0530

    Create string.js

---
+++ video 60jsstring/string.js
@@ -0,0 +1,28 @@
+console.log("This is string tutorials")
+let a = "Ramesh";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+
+let real_name = "Ravi"
+let Friend = "Rohan"
+
+console.log(" His name is " + real_name + " and his friends name is " + Friend)
+console.log(`His name is ${real_name} and his friends name is ${Friend}`)
+
+let b = "shivam"
+console.log(b.toUpperCase())
+console.log(b.charCodeAt())
+//console.log(b.codePointAt())
+console.log(b.localeCompare())
+console.log(b.length)
+console.log(b.slice(1, 4))
+console.log(b.at())
+console.log(b.replace("va", "85"))
+console.log(b.concat(a," prabhat ", " Rahul "))
