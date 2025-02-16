9d41b6c56d9aa7b698dcd54123504f26e68e1fe1
Raviprabhat527 <ravikumar312004@gmail.com>
2025-02-16 13:00:46 +0530

    DOM

---
+++ video 67DOM/script.js
@@ -0,0 +1,10 @@
+console.log("Hello world")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[3].parentElement
+document.body.children
+document.body.children[1]
+document.body.children[1].rows
