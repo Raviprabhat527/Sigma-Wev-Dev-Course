c89f40b46d39e9b8cf6bd7f9dfaa4ef1c99888dd
Ravi Kumar <ravikumar312004@gmail.com>
2025-03-27 16:53:15 +0530

    nodejs class
    nodejs class

---
+++ cartModule.js
@@ -0,0 +1,11 @@
+let addToCart=()=>{
+    return "Add to Cart"
+}
+
+let changeQty=()=>{
+    return 5;
+}
+
+//module.exports=addToCart
+
+module.exports={addToCart, changeQty}
--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-let http=require("http")
+const {addToCart, changeQty} = require("./cartModule")
 
-let server=http.createServer((req,res)=>{
+console.log("Welcom to ws")
 
-    if(req.url=="/news"){ //http://localhost:7500/news
-        let obj={
-            status:1,
-            data:[
-                {
-                    newsTitle:'ws',
-                    newDes:"WS Hello"
-                },
-                {
-                    newsTitle:'IIP',
-                    newDes:"IIP Hello"
-                }
-            ]
-        }
-        res.end(JSON.stringify(obj))
-
-    }
-
-    if(req.url=="/about"){ //http://localhost:7500/about
-
-    }
-
-    if(req.url=="/"){ //http://localhost:7500
-        res.end("Welcome to ws")
-    }
-
+console.log(40+50)
+let l=[10,20,30,40,50]
+l.forEach((value,index)=>{
+    console.log(value,index)
 })
-server.listen("7500") //http://localhost:7500
+console.log(addToCart());
+console.log(changeQty());
